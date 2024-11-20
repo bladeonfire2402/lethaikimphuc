a3f58ff60885033c1ebec322944d7109c29cc8c7
bladeonfire2402 <liemp966@gmail.com>
2024-11-21 03:30:20 +0700

    contact

--- src/screen/contactscreen/contactscreen.jsx
+++ src/screen/contactscreen/contactscreen.jsx
@@ -5,15 +5,29 @@ const ContactScreen=()=>{
             <h4 className="text-poppins text-center mt-5 text-primary">Feel free to leave me a message via the form, or you may reach me at lethaikimphuc@gmail.com <br/> Lets create something beautiful and impactful.</h4>
 
             <form>
-                <div className="row-span-2">
+                <div className="grid grid-cols-2 gap-12 mt-10">
                     <div className="flex flex-col">
-                        <label className="text-poppins">Name</label>
-                        <input type="text"/>
+                        <label className="text-poppins text-primary">Name</label>
+                        <input className="bg-thirdly px-2 py-2 rounded-sm mt-3" type="text"/>
                     </div>
                     <div className="flex flex-col">
-                        <label className="text-poppins">Name</label>
-                        <input type="text"/>
+                        <label className="text-poppins text-primary">Email Address</label>
+                        <input className="bg-thirdly px-2 py-2 rounded-sm mt-3" type="email"/>
                     </div>
+                </div>
+                <div className="grid grid-cols-2 gap-12 mt-10">
+                    <div className="flex flex-col">
+                        <label className="text-poppins text-primary">Phone Number</label>
+                        <input className="bg-thirdly px-2 py-2 rounded-sm mt-3" type="text"/>
+                    </div>
+                    <div className="flex flex-col">
+                        <label className="text-poppins text-primary">What's this regarding?</label>
+                        <input className="bg-thirdly px-2 py-2 rounded-sm mt-3" type="text"/>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                <label className="text-poppins text-primary mt-10">Message</label>
+                <input className="bg-thirdly px-2 py-10 rounded-sm mt-3" type="text" />
                 </div>
             </form>
         </div>
