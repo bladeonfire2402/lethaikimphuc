44eeb8c5dd26da2b78e8ee98be9859c83efdbd04
bladeonfire2402 <liemp966@gmail.com>
2024-12-07 23:02:06 +0700

    nn

--- src/screen/contactscreen/contactscreen.jsx
+++ src/screen/contactscreen/contactscreen.jsx
@@ -16,7 +16,7 @@ const ContactScreen=({witdh})=>{
       // Sử dụng hàm slowScrollUp
       slowScrollUp();
     return(
-        <div className="Contactscreen-wrapper pt-10">
+        <div className="Contactscreen-wrapper pt-10 res-pb">
             <h1 className="text-missing-love text-center text-secondary text-7xl ">Let’s work together</h1>
             <h4 className="text-poppins text-center mt-5 text-primary">Feel free to leave me a message via the form, or you may reach me at lethaikimphuc@gmail.com. {witdh>480?<br/>:<></>} Lets create something beautiful and impactful.</h4>
 
