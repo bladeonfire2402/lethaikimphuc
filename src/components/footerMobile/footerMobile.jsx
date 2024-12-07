a5ea5243c0525b70a930c2cbf032371f3d9df55a
bladeonfire2402 <liemp966@gmail.com>
2024-12-07 22:27:34 +0700

    fix link

--- src/components/footerMobile/footerMobile.jsx
+++ src/components/footerMobile/footerMobile.jsx
@@ -1,9 +1,10 @@
 
 import './index.css'
 import { AccessPage } from '../../func/access'
+import { useNavigate } from 'react-router-dom'
 
 const FooterMobile=()=>{
-  
+  const navigate=useNavigate()
     return(
         <div className="FooterMobile-wrapper bg-primary py-14 flex flex-col  ">
             <div className="flex justify-center"><img alt="" src={`${process.env.PUBLIC_URL}/assets/img/main/whitelogo.png`}/></div>
@@ -11,11 +12,11 @@ const FooterMobile=()=>{
                 <h2 className="uppercase text-gilroy-bold text-white">Quick Link</h2>
                 <div className='grid grid-cols-2 text-poppins text-white mt-11 gap-7 cursor-pointer'>
                     <nav>Home</nav>
-                    <nav className='-ml-12'>Projects</nav>
-                    <nav>Services</nav>
+                    <nav className='-ml-12 ' onClick={()=>{navigate('/project')}}>Projects</nav>
+                    <nav  onClick={()=>{navigate('service')}}>Services</nav>
                     <nav className='-ml-12'>Portfolio</nav>
-                    <nav>About</nav>
-                    <nav className='-ml-12'>Contact</nav>
+                    <nav onClick={()=>{navigate('/about')}}>About</nav>
+                    <nav className='-ml-12' onClick={()=>{navigate('/contact')}}>Contact</nav>
                 </div>
             </div>
             <div className='Contacts px-14 mt-8'>
